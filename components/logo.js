8444da4a4e115e4dc1cc3bcc7b670b66d3a77216
BertiRean <tplaza15@gmail.com>
2022-10-06 17:49:57 -0300

    Components: Logo - Agregado Logo y Texto

---
+++ components/logo.js
@@ -0,0 +1,23 @@
+import { Image } from "@rneui/themed"
+import { Text } from "@rneui/themed"
+import { View } from "react-native"
+import { colorPalette } from "../styles/colors"
+
+export function Logo(props) {
+    return (
+        <View style={{flexDirection : 'column', alignItems :"center"}}>
+            <Image 
+                source={require('../assets/logo.png')}
+                style = {{
+                    width : 185,
+                    height : 162,
+                    alignSelf : 'stretch'
+                }}
+            >
+            </Image>
+            <Text h2 h2Style={{color : colorPalette.Black}}>
+                Morfando
+            </Text>
+        </View>
+        )
+}
