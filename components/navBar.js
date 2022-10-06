c94c91117644a63d743e7fdab2fb0f66e6e5b263
BertiRean <tplaza15@gmail.com>
2022-10-06 18:39:03 -0300

    Screen: LoginScreen - Completada Pantalla sin Google Sign In (To Do: Necesitamos Componente de Google Sign In)

--- components/navBar.js
+++ components/navBar.js
@@ -1,9 +1,10 @@
 import { Header } from "@rneui/themed";
 import { colorPalette } from "../styles/colors";
 
-export default function NavBar(props) {
+export function NavBar(props) {
     return (
         <Header
+            containerStyle={{borderWidth : 1}}
             backgroundColor={colorPalette.Orange}
             centerComponent={{text : props.centerText, style : {color : colorPalette.Black, fontFamily : 'Roboto'}}}
             leftComponent={{icon : props.leftIcon, color : colorPalette.Black, type : 'material'}}
---
+++ screens/LoginScreenUser.js
@@ -0,0 +1,23 @@
+import { View } from "react-native";
+import { Logo } from "../components/Logo";
+import { NavBar } from "../components/navBar";
+import { colorPalette } from "../styles/colors";
+import { Text } from "@rneui/themed";
+import { MyButton } from "../components/button";
+
+export function LoginUserScreen(props){
+    return (
+        <View style={{flexDirection : 'column', alignItems : 'center', marginTop : 23}}>
+            <NavBar
+                centerText = 'Inicio de Sesión'
+                leftIcon = 'arrow-back'
+            ></NavBar>
+            <View style={{height : 40, backgroundColor : colorPalette.White}}></View>
+            <Logo></Logo>
+            <View style={{height : 40, backgroundColor : colorPalette.White}}></View>
+            <Text h2 h2Style={{color : colorPalette.Orange}}>Iniciar sesión</Text>
+            <View style={{height : 40, backgroundColor : colorPalette.White}}></View>
+            <MyButton title = 'Placeholder Google Sign In'></MyButton>
+        </View>
+    )
+}
