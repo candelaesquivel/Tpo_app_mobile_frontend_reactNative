7833846a0c32bfe9c18b9f53f11ba895001ef574
Candela Esquivel <80803466+candelaesquivel@users.noreply.github.com>
2022-10-06 15:47:53 -0300

    Componente de restaurante de usuario realizado. Splash : cover , actualizado.

---
+++ components/restaurantCardUser.js
@@ -0,0 +1,39 @@
+import React from 'react';
+import { View, Image } from 'react-native';
+import { Text, Card, Icon } from '@rneui/themed';
+
+export default function RestaurantCardUser(props) {
+
+  return (
+    <View >
+    <Card>
+      <View style={{flexDirection: 'row', justifyContent : 'space-between'}}>
+      <Image
+        style={{height:100, width:100, resizeMode:'stretch'}}
+         source={require('../assets/logo.png')}
+       >
+       </Image>
+       <View width={200}  style={{justifyContent : 'space-evenly'}} >
+         <Text h4 >{props.name}</Text>
+         <Text>{props.address}</Text>
+         <View style={{flexDirection: 'row',alignItems: 'center'}}>
+            <Text marginBottom={30}>{props.score} </Text>
+            <Icon  name="star" color="#FF511B" size={20}></Icon>
+         </View>
+       
+       </View>
+       <View  style= {{flexDirection:'column-reverse'}}>
+        {
+            props.favorite ? <Icon  name="favorite" color="red"  marginBottom={7}></Icon >
+            : <Icon  name="favorite-border" color="#404040"  marginBottom={7}></Icon >
+        }
+       </View>
+      
+      </View>
+         </Card>
+   
+     
+ </View>
+  )
+}
+
