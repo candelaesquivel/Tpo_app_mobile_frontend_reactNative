d327d66b779d92a85eebaa321cdc16ba46ab603b
BertiRean <tplaza15@gmail.com>
2022-10-06 19:45:38 -0300

    Screen: Agregada pantalla de Email Enviado con instrucciones de contraseña

---
+++ screens/EmailSentScreen.js
@@ -0,0 +1,28 @@
+import { View } from "react-native";
+import { NavBar } from "../components/navBar";
+import { colorPalette } from "../styles/colors";
+import { Text } from "@rneui/themed";
+import { MyButton } from "../components/button";
+import { Icon } from "@rneui/themed";
+import { InputText } from "../components/textField";
+import { color } from "@rneui/base/dist";
+
+export function EmailSentScreen(props){
+    return (
+        <View style={{flexDirection : 'column', alignItems : 'center', marginTop : 23}}>
+            <NavBar
+                centerText = 'Email Enviado'
+                leftIcon = 'arrow-back'
+            ></NavBar>
+            <View style={{height : '35%', backgroundColor : colorPalette.White}}></View>
+            <Icon name = 'outgoing-mail' size={96} color={colorPalette.Orange}></Icon>
+            <Text
+                h4
+                h4Style={{textAlign:'center'}}
+                style={{marginBottom : 10}}
+            >
+                Se ha enviado a el email las instrucciones de recuperación
+            </Text>
+        </View>
+    )
+}
