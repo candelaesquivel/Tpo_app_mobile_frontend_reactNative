d2f19e47493ee1e69e230b2758fd9c9d759f732d
BertiRean <tplaza15@gmail.com>
2022-10-06 10:37:20 -0300

    Agregada React Native Elements

--- App.js
+++ App.js
@@ -1,11 +1,11 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, View } from 'react-native';
+import { Text, Button, Header} from '@rneui/themed';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Header leftComponent={{icon : "menu"}}></Header>
     </View>
   );
 }
