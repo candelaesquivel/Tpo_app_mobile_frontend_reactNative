1033195dde62a71427523c8504d6f578bb2bb2cb
Candela Esquivel <80803466+candelaesquivel@users.noreply.github.com>
2022-10-06 17:52:10 -0300

    MenuCard creado.

---
+++ components/menuCard.js
@@ -0,0 +1,44 @@
+import React from 'react';
+import { View, Image } from 'react-native';
+import { Text, Card, Icon } from '@rneui/themed';
+import { colorPalette } from '../styles/colors';
+
+
+
+
+export default function MenuCard(props) {
+    const priceDescount =(props.price)* ((100-props.discount)/100)
+  return (
+    <View >
+    <Card>
+      <View style={{flexDirection: 'row', justifyContent : 'space-between'}}>
+      <Image
+        style={{height:100, width:100, resizeMode:'stretch'}}
+         source={require('../assets/logo.png')}
+       >
+       </Image>
+       <View width={200}  style={{justifyContent : 'space-evenly'}} >
+         <Text h4 >{props.name}</Text>
+         <Text style={{color: colorPalette.Orange ,textDecorationLine: 'line-through' , fontSize: 18}}> ${props.price}</Text>
+         <View style={{flexDirection: 'row',alignItems: 'center'}}>
+            <Text style={{color: colorPalette.Black , fontSize: 18}} marginBottom={30}> ${priceDescount} </Text>
+            <Icon  name="local-offer" color={colorPalette.Orange} size={20}></Icon>
+            <Text style={{color: colorPalette.Orange , fontSize: 15}} marginBottom={30}>%{10}</Text>
+            
+         </View>
+         <View style={{flexDirection: 'row-reverse',alignItems: 'center'}} >
+            <Icon name="leaf" type='font-awesome-5' color={colorPalette.Black} size={20}></Icon>
+            <Icon name="leaf" type='font-awesome-5' color={colorPalette.Black} size={20}></Icon>
+           
+        </View>
+       
+       </View>
+     
+      
+      </View>
+         </Card>
+   
+     
+ </View>
+  )
+}
