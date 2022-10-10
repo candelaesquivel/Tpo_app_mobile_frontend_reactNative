623ead0530e5189b43e58e4beb8f899ac0c61caf
Candela Esquivel <80803466+candelaesquivel@users.noreply.github.com>
2022-10-10 19:40:35 -0300

    Imagen logo SVG creado.

--- ui/components/logo.js
+++ ui/components/logo.js
@@ -2,19 +2,12 @@ import { Image } from "@rneui/themed"
 import { Text } from "@rneui/themed"
 import { View } from "react-native"
 import { colorPalette } from "../styles/colors"
+import Images from '../../assets/images/index';
 
 export function Logo(props) {
     return (
         <View style={{flexDirection : 'column', alignItems :"center"}}>
-            <Image 
-                source={require('../assets/logo.png')}
-                style = {{
-                    width : 185,
-                    height : 162,
-                    alignSelf : 'stretch'
-                }}
-            >
-            </Image>
+            <Images.logo width={185} height={162}></Images.logo>
             <Text h2 h2Style={{color : colorPalette.Black}}>
             {I18n.t('appName')}
             </Text>
