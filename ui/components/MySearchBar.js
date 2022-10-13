651323bdb3757ca406d9db414465f8720dfd4bbc
Candela Esquivel <80803466+candelaesquivel@users.noreply.github.com>
2022-10-13 18:37:50 -0300

    search bar mejorada.

--- ui/components/MySearchBar.js
+++ ui/components/MySearchBar.js
@@ -9,7 +9,7 @@ import { Input } from '@rneui/base';
 export default function MySearchBar() {
 
   return (
-    <View style={{alignItems : 'center'}}>
+    <View style={{alignItems : 'center' , width: '100%' , marginTop: 30}}>
 
       <Input
         leftIcon={<Icon name = 'search' color={colorPalette.White}></Icon>}
