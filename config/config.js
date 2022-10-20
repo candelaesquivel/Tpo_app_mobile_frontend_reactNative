e0742f1baf1a9591e207a3e6d9a2b206b3fe85b3
BertiRean <tplaza15@gmail.com>
2022-10-20 15:02:00 -0300

    Config: Agregadas rutas para llamar al backend

---
+++ config/config.js
@@ -0,0 +1,24 @@
+const URL_API = "http://10.0.2.2:8080/api/v1/";
+console.log("url",URL_API);
+
+const RESTAURANT_ROUTE = URL_API + '/restaurants/';
+const USER_ROUTE = URL_API + '/users/'
+
+const URL_SERVICES = {
+  // AUTH
+  REGISTER_OWNER : USER_ROUTE + 'register',
+  LOGIN : USER_ROUTE + 'login',
+  LOGOUT : USER_ROUTE + 'logout',
+  RECOVER_PASSWORD : USER_ROUTE + 'recoverPassword',
+
+  // User Routes
+  GET_USER_INFO : USER_ROUTE + '',
+  UPLOAD_USER_IMAGE : USER_ROUTE + '/image',
+  FAVORITE_RESTAURANTS_NORMAL_USER : USER_ROUTE + 'favorites',
+  RESTAURANTS_OWNER : USER_ROUTE + 'restaurants',
+
+  // Status Server check
+  HEALTH_CHECK : URL_API + 'health',
+}
+
+export default URL_SERVICES;
