58ef3609ae04b919850cb7e704e8f27d03b0e5f5
candelaesquivel <candelaesquivelelizondo@outlook.com>
2022-10-29 18:18:59 -0300

    Restaurant cardowner componente arreglado.

--- ui/components/RestaurantCardOwner.js
+++ ui/components/RestaurantCardOwner.js
@@ -1,39 +1,83 @@
 import React from 'react';
-import { View } from 'react-native';
+import { View , StyleSheet , Dimensions} from 'react-native';
 import { Text, Card, Icon } from '@rneui/themed';
 import { colorPalette } from '../styles/colors';
 import I18n from "../../assets/localization/I18n";
 import Images from '../../assets/images/index';
-
+import { Theme } from '../styles/Theme';
 function RestaurantCardOwner({name = 'Rodizio', 
+
 address = 'Honduras 5500', score = 5, onMenuPressed = {}, onPhotoPress = {},
 props}) {
 
   return (
-    <View style={{width:'100%'}} >
-    <Card>
-      <View style={{flexDirection: 'row', justifyContent : 'space-between'}}>
-      <Images.logo onPress={onPhotoPress} width={100} height={100}></Images.logo>
-       <View width={200}  style={{justifyContent : 'space-evenly'}} >
-         <Text h4 >{name}</Text>
-         <Text>{address}</Text>
-         <View style={{flexDirection: 'row',alignItems: 'center'}}>
+    <View style={{width:'100%' , height : "100%"}} >
+      <Card>
+        <View style={styles.global}>
+
+         <Images.logo
+          onPress={onPhotoPress} 
+          width={"30%"}
+          height={"90%"}
+          ></Images.logo>
+
+          <View width={"60%"}  style={styles.globalTwo} >
+
+            <Text style={styles.title} >{name}</Text>
+            <Text style={styles.address}>{address}</Text>
+
+          <View style={styles.globalThree}>
             <Text marginBottom={30}>{score} </Text>
             <Icon name="star" color={colorPalette.Orange} size={20}></Icon>
+          </View>
+
+          <View style={styles.globalFour} >
+            <Text  onPress={onMenuPressed} style={styles.menu} >{I18n.t('menu')}</Text>
+          </View>
+
          </View>
-         <View style={{direction: 'RLT',alignItems: 'flex-end'}} >
-                <Text  onPress={onMenuPressed} style={{fontWeight: 'bold', fontSize:18 }} >{I18n.t('menu')}</Text>
-            </View>
-       
-       </View>
-     
       
-      </View>
-         </Card>
-   
-     
+        </View>
+      </Card>
+    
  </View>
   )
 }
 
 export default RestaurantCardOwner;
+
+const styles = StyleSheet.create({
+  global : {
+    flexDirection: 'row', 
+    justifyContent : 'space-between' , 
+    width : "90%"
+  },
+  globalTwo : {
+    justifyContent : 'space-evenly',
+    marginLeft : "10%"
+  },
+  globalThree : {
+    flexDirection: 'row',
+    lignItems: 'center'
+  },
+  globalFour :{
+    direction: 'RLT',
+    alignItems: 'flex-end',
+    
+  },
+  title :{
+    fontSize: Theme.font.MEDIUM,
+    color: colorPalette.Black, 
+    fontWeight: 'bold', 
+   
+},
+address :{
+  fontSize: Theme.font.SMALL,
+  color: colorPalette.Black, 
+ 
+},
+menu :{
+  fontWeight: 'bold', 
+  fontSize: Theme.font.MEDIUM
+ },
+});
