8deb275f3a741a599fe50c135e202f6f1ed36163
candelaesquivel <candelaesquivelelizondo@outlook.com>
2022-10-29 20:48:39 -0300

    comment arreglado.

--- ui/components/comment.js
+++ ui/components/comment.js
@@ -1,35 +1,43 @@
 import React from 'react';
-import { View, Image } from 'react-native';
+import { View, Image , StyleSheet , Dimensions} from 'react-native';
 import { Text, Card, AirbnbRating } from '@rneui/themed';
 import { colorPalette } from '../styles/colors';
 import I18n from "../../assets/localization/I18n";
-
+import { Theme } from '../styles/Theme';
 
 
 export default function Comment(props) {
 
   return (
-    <View  >
-      
-    <Card  containerStyle={{backgroundColor : colorPalette.Cream , alignItems: 'center'}}>
-      <View style={{flexDirection: 'row', justifyContent : 'space-between'}}>
-       <View  style={{alignItems: 'center'}} >
+    <View>  
+    <Card  containerStyle={styles.global}>
+       <View  style={{alignItems: 'center' }} >
          <Text h4 style={{color: colorPalette.Black}}>{props.userName }</Text>
-         <Text>{props.comment}</Text>
-         <Text>{I18n.t('vegan')}</Text>
+         <Text style={styles.words}>{props.comment}</Text>
+         <Text style={styles.words}>{I18n.t('vegan')}</Text>
          <AirbnbRating 
-                defaultRating={props.score}
-                reviews = {[]}
-                size = {20}
-                selectedColor = {colorPalette.Orange}
-            ></AirbnbRating> 
+          defaultRating={props.score}
+          reviews = {[]}
+          size = {20}
+          selectedColor = {colorPalette.Orange}></AirbnbRating> 
        </View>
-      
-      </View>
-         </Card>
-   
-     
+    </Card>     
  </View>
   )
 }
 
+const styles = StyleSheet.create({
+  global:{
+    backgroundColor : colorPalette.Cream , 
+    alignItems: "center",
+    width  : Dimensions.get("window").width*0.9,
+    height : "60%"
+  },
+  words :{
+    fontSize: Theme.font.MEDIUM,
+    color: colorPalette.Black, 
+  
+},
+    
+ 
+});
