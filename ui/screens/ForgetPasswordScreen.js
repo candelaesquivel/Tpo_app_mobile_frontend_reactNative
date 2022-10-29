70784938383ca1c14324a4c905ad9b2fa9814ade
candelaesquivel <candelaesquivelelizondo@outlook.com>
2022-10-29 16:45:55 -0300

    Forgetpassword finalizado el front.

--- ui/screens/ForgetPasswordScreen.js
+++ ui/screens/ForgetPasswordScreen.js
@@ -1,14 +1,15 @@
 import { color, Icon } from "@rneui/base"
 import { useEffect } from "react"
 import I18n from "../../assets/localization/I18n"
-import { View } from "react-native"
+import { View , StyleSheet } from "react-native"
 import { colorPalette } from "../styles/colors"
 import { MyButton } from "../components/button"
-import { TextInput } from "react-native"
+import { TextInput , Dimensions} from "react-native"
 import { Input } from "@rneui/themed"
 import { InputText } from "../components/InputText"
 import recoverPass from "../../networking/recoverPass"
 import { useState } from "react"
+import { Theme } from "../styles/Theme"
 
 function ForgetPasswordScreen({navigation, props}) {
 
@@ -25,25 +26,67 @@ function ForgetPasswordScreen({navigation, props}) {
     const onRecoveryTouch = async (event) => {
       let recoveryResult = await recoverPass(email);
 
-      console.log(recoveryResult);
     }
 
     return (
-        <View style={{flexDirection : 'column', 
-        height : '100%',
-        alignItems : 'center', backgroundColor : colorPalette.White}}>
-            <View style={{width : '100%', height : '15%', backgroundColor : colorPalette.White}}></View>
+        <View style={styles.global}>
+          <View style={styles.containerWhite}>
             <Icon name='lock' size={96} color={colorPalette.Orange}></Icon>
-            <View style={{width : '100%', height : '5%', backgroundColor : colorPalette.White}}></View>
-
-            <View style={{ justifyContent : 'space-evenly', height : '10%', width : '80%', backgroundColor : colorPalette.LightOrange, borderRadius : 30}}>
-                <InputText onChange = {onEmailChange} placeholder = {I18n.t('emailInput')} color={colorPalette.Orange}></InputText>
-            </View>
-            <View style={{width : '100%', height : '5%', backgroundColor : colorPalette.White}}></View>
-            <MyButton title = {I18n.t('recoverPasswordBtn')} onPress = {onRecoveryTouch}></MyButton>
-        </View>
+          </View>
+         <View style={styles.containerEmail}>
+          <View style={styles.containerEmailTwo}>
+          <InputText 
+            color = {colorPalette.Orange} 
+            placeholder = {I18n.t('emailInput')}
+            placeholderTextColor ={colorPalette.White}
+            height = '50%' 
+            onChange={onEmailChange}
+            ></InputText>
+          </View> 
+          </View>
+          <View style={styles.button}>
+            <MyButton 
+            title = {I18n.t('recoverPasswordBtn')} 
+            onPress = {onRecoveryTouch}
+            width={ Dimensions.get("window").width*0.6}
+            height={Dimensions.get("window").height*0.07}
+            ></MyButton>
+          </View>
+         </View>
     )
 
 }
 
 export default ForgetPasswordScreen;
+
+const styles = StyleSheet.create({
+ global :{
+  flexDirection : 'column', 
+  alignItems : 'center', 
+  backgroundColor : colorPalette.White,
+  height : "100%"
+ },
+ containerWhite : {
+    width : '100%',
+   height : '15%',
+   backgroundColor : colorPalette.White,
+   marginTop : "35%",
+   marginBottom : "5%"
+  },
+  containerEmail : { 
+    justifyContent : 'space-evenly',
+     height : '10%',
+      width : '90%', 
+     backgroundColor : colorPalette.LightOrange, 
+     borderRadius : Theme.sizes.ROUNDED
+    },
+  containerEmailTwo : { 
+    flexDirection : "column",
+      marginTop : "7%"
+    },
+  button : {
+    flexDirection :"column",
+    justifyContent : "center",
+    marginTop : "5%"
+  },
+});
