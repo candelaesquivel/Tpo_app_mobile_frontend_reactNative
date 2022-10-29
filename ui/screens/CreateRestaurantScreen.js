a3e7eb8ab45ce4d8d1be24646daaeff370c4de3e
candelaesquivel <candelaesquivelelizondo@outlook.com>
2022-10-28 23:35:50 -0300

    Crear restaurante mejorado todo el front , lista para conectar con backend.

--- ui/screens/CreateRestaurantScreen.js
+++ ui/screens/CreateRestaurantScreen.js
@@ -1,6 +1,5 @@
-import { View, Text , ScrollView , StyleSheet} from 'react-native'
+import { View, Text , ScrollView , StyleSheet , Dimensions} from 'react-native'
 import React, { useState } from 'react'
-import {  Switch } from '@rneui/base'
 import { colorPalette } from '../styles/colors'
 import I18n from "../../assets/localization/I18n";
 import { InputText } from '../components/InputText'
@@ -11,8 +10,13 @@ import Mapa from '../components/mapa';
 import { Dropdown } from 'react-native-element-dropdown';
 import MyWeekButtons from '../components/WeekButton';
 import MyTimePicker from '../components/TimePicker';
+import { Theme } from '../styles/Theme';
+import CloseComponent from '../components/closeComponent';
 
 function CreateRestaurantScreen({navigation, props}) {
+  
+  const [openingHour,setOpeningHour] = useState(false);
+  const [closingHour,setClosingHour] = useState(false);
 
   const data = [
     { label: 'Mexicana', value: '1' },
@@ -28,21 +32,6 @@ function CreateRestaurantScreen({navigation, props}) {
     { label: '$$$$', value: '4' },
    
   ];
-  const [value, setValue] = useState(null);
-  const [isFocus, setIsFocus] = useState(false);
-  const [openingHour,setOpeningHour] = useState(false);
-  const [closingHour,setClosingHour] = useState(false);
-
-  const renderLabel = () => {
-    if (value || isFocus) {
-      return (
-        <Text style={[styles.label, isFocus && { color: 'blue' }]}>
-          Dropdown label
-        </Text>
-      );
-    }
-    return null;
-  };
 
   const onCreateRestaurantPressed = (event) => {
     console.log('On Restaurant Create Press');
@@ -51,184 +40,134 @@ function CreateRestaurantScreen({navigation, props}) {
 
   return (
     <ScrollView>
-    <Carousal></Carousal>
-    <View style={{flexDirection:"row-reverse"}}>
-    <Icon name = 'add' size={30} style={{marginRight: 10 , marginTop : 10 }}></Icon>
-    </View>
+     <Carousal></Carousal>
+      <View style={{flexDirection:"row-reverse"}}>
+        <Icon name = 'add' size={30} style={{marginRight: 10 , marginTop : "5%" }}></Icon>
+       </View>
+        <View style={styles.global}>
 
-    <View style={{flexDirection : 'column', alignItems : 'center', marginTop : 20}}>
- 
-        <View style={{width:'85%', alignItems:'flex-start'}}>
-            <Text
-            style={{ fontSize: 20, color: colorPalette.Black}}
-            >
-                {I18n.t('name')}    
-            </Text>
-            <InputText 
-            placeholder=""
-             
-            color={colorPalette.White}
-            placeholderTextColor = {colorPalette.Black}
-            ></InputText>
+                <View style={styles.globalTwo}>
 
-            <Text
-            style={{ fontSize: 20, color: colorPalette.Black}}
-            >
-                Direccion   
-            </Text>
-            <InputText 
-                placeholder=""
-                 
-                color={colorPalette.White}
-                placeholderTextColor = {colorPalette.Black}
-            ></InputText>
-
-            <Text
-            style={{ fontSize: 20, color: colorPalette.Black}}
-            >
-                Barrio    
-            </Text>
-            <InputText 
-                placeholder=""
-                 
-                color={colorPalette.White}
-                placeholderTextColor = {colorPalette.Black}
-            ></InputText>
-
-            <Text
-            style={{ fontSize: 20, color: colorPalette.Black}}
-            >
-                Localidad   
-            </Text>
-
-            <InputText 
-                placeholder=""
-                 
-                color={colorPalette.White}
-                placeholderTextColor = {colorPalette.Black}
-            ></InputText>
-            
-            <Text
-            style={{ fontSize: 20, color: colorPalette.Black}}
-            >
-                Codigo postal   
-            </Text>
-
-            <InputText 
-                placeholder=""
-                 
-                color={colorPalette.White}
-                placeholderTextColor = {colorPalette.Black}
-            ></InputText>
-            
-               
-
-            </View>
-          
-            <View style={{height:'3%'}}></View>
-            <View style={{flexDirection:'row' }}>
-            <Text
-                    style={{fontSize: 20, color: colorPalette.Black, width:'45%'}}
-                    >
-                    Cerrado  
+                    <Text style={styles.words}>
+                        {I18n.t('name')}    
                     </Text>
-                    <Switch
-                    value={false}
-                    ></Switch>
-            </View>
-        
-            <View style={{height:'3%'}}></View>  
-        
+                    <InputText 
+                    placeholder=""
+                    color={colorPalette.White}
+                    placeholderTextColor = {colorPalette.Black}
+                    ></InputText>
 
+                    <Text style={styles.words}>
+                        {I18n.t('address')}    
+                    </Text>
+                    <InputText 
+                    placeholder=""
+                    color={colorPalette.White}
+                    placeholderTextColor = {colorPalette.Black}
+                    ></InputText>
+
+                    <Text  style={styles.words} >
+                     {I18n.t('neighborhood')}    
+                    </Text>
+                    <InputText 
+                    placeholder=""
+                    color={colorPalette.White}
+                    placeholderTextColor = {colorPalette.Black}
+                    ></InputText>
+
+                    <Text style={styles.words}  >
+                    {I18n.t('location')}    
+                    </Text>
+                    <InputText 
+                    placeholder=""
+                    color={colorPalette.White}
+                    placeholderTextColor = {colorPalette.Black}
+                    ></InputText>
+                    
+                    <Text style={styles.words}
+                    >
+                     {I18n.t('zipCode')}    
+                    </Text>
+                    <InputText 
+                    placeholder=""
+                    color={colorPalette.White}
+                    placeholderTextColor = {colorPalette.Black}
+                    ></InputText>
+
+                  <Text style={styles.words}>
+                    {I18n.t('hour')} 
+                    </Text>
+
+                </View>
+          <View style={styles.hour}>
+          
+                < MyButton
+                title= {I18n.t('opening')} 
+                width={ Dimensions.get("window").width*0.5}
+                height={Dimensions.get("window").height*0.07}
+                onPress={() =>{ setOpeningHour(!openingHour)}}
+                ></MyButton>
+
+                < MyButton
+                title= {I18n.t('closing')} 
+                width={ Dimensions.get("window").width*0.5}
+                height={Dimensions.get("window").height*0.07}
+                onPress={() => {setClosingHour(!closingHour)}}
+                ></MyButton>
+              
+                  {
+                      openingHour && <MyTimePicker></MyTimePicker>
+                  }
+                  
+                  {
+                      closingHour &&  <MyTimePicker></MyTimePicker>
+                  }
+                  
+                </View>
+               
           <Mapa></Mapa>
-                     
-        <View style={{flexDirection : "column"}}>
-        <Text
-             style={{fontSize: 20, color: colorPalette.Black, width:'45%'}}
-             >
-                Horario 
-            </Text>
-            < MyButton
-            title= "Apertura"
-            width={200}
-            height={50}
-            onPress={() =>{ setOpeningHour(!openingHour)}}
-            ></MyButton>
-
-            < MyButton
-            title= "Cierre"
-            width={200}
-            height={50}
-            onPress={() => {setClosingHour(!closingHour)}}
-            ></MyButton>
-            <MyTimePicker></MyTimePicker>
-
-            {
-                openingHour && <MyTimePicker></MyTimePicker>
-            }
-            
-            {
-                closingHour &&  <MyTimePicker></MyTimePicker>
-            }
-            
-        </View>
-          <View style ={{width : "90%" , marginBottom : 10  }}>
-
-          <Dropdown
-          style={[styles.dropdown ]}
-          placeholderStyle={styles.placeholderStyle}  
-          itemTextStyle ={styles.placeholderStyle}       
-          data={data}
-          labelField="label"
-          valueField="value"
-          placeholder={ 'Tipo de comida'}
-          onFocus={() => setIsFocus(true)}
-         
-          onChange={item => {
-            setValue(item.value);
-            setIsFocus(false);
-          }}
-        
-        />
+          <CloseComponent>s</CloseComponent>
+          <View style ={styles.dropdownContainer}>
+                <Dropdown
+                style={[styles.dropdown ]}
+                placeholderStyle={styles.placeholderStyle}  
+                itemTextStyle ={styles.placeholderStyle}       
+                data={data}
+                labelField="label"
+                valueField="value"
+                placeholder={ 'Tipo de comida'}              
+                       
+              />
       
           </View>
-          <View style ={{width : "90%" , marginBottom : 10  }}>
+         <View style ={styles.dropdownContainer}>
 
-          <Dropdown
-          style={[styles.dropdown ]}
-          placeholderStyle={styles.placeholderStyle}  
-          itemTextStyle ={styles.placeholderStyle}   
-          selectedTextStyle={styles.selectedTextStyle} 
-          data={data2}
-          labelField="label"
-          valueField="value"
-          placeholder={ 'Precio'}
-          onFocus={() => setIsFocus(true)}
-         
-      
-          onChange={item => {
-            setValue(item.value);
-           
-          }}
-          activeColor={colorPalette.Orange}
-        />
+              <Dropdown
+              style={[styles.dropdown ]}
+              placeholderStyle={styles.placeholderStyle}  
+              itemTextStyle ={styles.placeholderStyle}   
+              labelField="label"
+              valueField="value"
+              data={data2}
+              placeholder={ 'Precio'}
+            />
+          </View>
+          
+          <MyWeekButtons></MyWeekButtons>
+
+          
+          <View style={{flexDirection: 'column' , width : "100%",height : "60%", alignItems : "center" }}>
+            < MyButton
+                title= "Crear"
+                width={ Dimensions.get("window").width*0.5}
+                height={Dimensions.get("window").height*0.07}
+                ></MyButton>
+
+           </View>
+       
       </View>
-      
-      <MyWeekButtons></MyWeekButtons>
-      
-        <View style={{flexDirection: 'column' , width : "100%",height : 160 , alignItems : "center" }}>
 
-        < MyButton
-            title= "Crear"
-            width={200}
-            height={50}
-            ></MyButton>
-    
-            </View>
-           
-                 
-    </View>
-    
+      
 </ScrollView>
   )
 }
@@ -236,6 +175,31 @@ function CreateRestaurantScreen({navigation, props}) {
 export default CreateRestaurantScreen;
 
 const styles = StyleSheet.create({
+    globalTwo:{
+      width:'90%', 
+      alignItems:'flex-start'
+    },
+    global:{
+      flexDirection : 'column',
+      alignItems : 'center'
+    },
+    words :{
+      fontSize: Theme.font.MEDIUM,
+      color: colorPalette.Black, 
+      marginLeft : "4%" , 
+      marginBottom : "3%"
+  },
+  hour : {
+    flexDirection : "column" , 
+    alignSelf : "center" ,
+    marginBottom :"5%"
+  }
+  ,
+  dropdownContainer: {
+    width : "90%" , 
+    marginBottom : 10  
+   
+  },
   container: {
     backgroundColor: colorPalette.White,
     padding: 10,
@@ -247,13 +211,11 @@ const styles = StyleSheet.create({
     borderRadius: 5,
     paddingHorizontal: 10, borderWidth:1,
   },
-  
-  
+   
   placeholderStyle: {
-    fontSize: 20,
+    fontSize: Theme.font.MEDIUM,
     color : colorPalette.Black
   },
 
- 
- 
 });
+
