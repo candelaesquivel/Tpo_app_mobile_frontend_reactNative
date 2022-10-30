7e2ec9612aa624b1e75d84c10230269f66833aca
BertiRean <tplaza15@gmail.com>
2022-10-30 14:03:35 -0300

    UserFavoriteRestaurantsScreen: Ahora obtendra los favoritos del usuario logeado actualmente.

--- ui/screens/UserFavoriteRestaurantsScreen.js
+++ ui/screens/UserFavoriteRestaurantsScreen.js
@@ -4,20 +4,23 @@ import MySearchBar from '../components/MySearchBar'
 import screenNames from '../screenNames'
 import { useState } from 'react';
 import I18n from '../../assets/localization/I18n'
-import { MyButton } from '../components/button';
 import { GetFavoriteRestaurants } from '../../networking';
 import { RestaurantFlatListUser } from '../components/RestaurantFlatListUser';
-
+import { useDispatch, useSelector } from 'react-redux';
 
 function UserFavoritesRestaurantsScreen({navigation , props}) {
   
   const [restaurants, setRestaurants] = useState([]);
   const [triggerSearch, setTrigggerSearch] = useState(false);
 
-  const [isFavorite, setFavorite] = useState(true);
+  const dispatch = useDispatch();
+  const userId = useSelector(state => state.session.userId);
+  const restaurantSelectedId = useSelector(state => state.session.restaurantSelectedId);
+
+  console.log("Curre Rest Id: ", restaurantSelectedId);
 
   const fillFavoriteRestaurantList = async () => {
-    const restos = await GetFavoriteRestaurants('635ddf0387204979748381de');
+    const restos = await GetFavoriteRestaurants(userId);
     setRestaurants(restos);
   }
 
@@ -40,17 +43,6 @@ function UserFavoritesRestaurantsScreen({navigation , props}) {
   const onRestaurantNameTouched = (event) => {
     console.log('On Restaurant Name Touched');
     navigation.navigate(screenNames.RESTAURANT_VIEW_USER);
-  }
-
-  const onFavoriteTouched = (event) => {
-    console.log('On Favorite Pressed');
-    setFavorite(!isFavorite);
-  }
-
-  const onGetPressed = async (event) => {
-    const restos = await GetFavoriteRestaurants('635ddf0387204979748381de');
-    console.log(restos);
-    setRestaurants(restos);
   }
 
   return (
