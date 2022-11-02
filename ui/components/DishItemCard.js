498ad962de0f9e3f33bfc205940aa7574dc578f0
BertiRean <tplaza15@gmail.com>
2022-11-02 01:44:34 -0300

    UI: Dish Item Card - Ahora manda los params del dish hacia la vista de ver plato

--- ui/components/DishItemCard.js
+++ ui/components/DishItemCard.js
@@ -25,7 +25,7 @@ export default function DishItemCard({name = '', discount = 0, price = 100, dish
     const dishData = await getDishData(restaurantId, dishId);
 
     if (role === CONSTANTS.ROLES.USER_ROLE)
-      navigation.navigate(ROUTES.DISH_USER_VIEW_STACK);
+      navigation.navigate(ROUTES.DISH_USER_VIEW_STACK, dishData);
     else if (role === CONSTANTS.ROLES.OWNER_ROLE)
       navigation.navigate(ROUTES.DISH_MODIFY_STACK, dishData);
   }
