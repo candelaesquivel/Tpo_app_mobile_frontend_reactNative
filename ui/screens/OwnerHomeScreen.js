7f5b931a72028e88c194c97df610cb6ceccf574e
BertiRean <tplaza15@gmail.com>
2022-11-03 13:22:39 -0300

    UI: Owner Home - Removidos unused

--- ui/screens/OwnerHomeScreen.js
+++ ui/screens/OwnerHomeScreen.js
@@ -13,7 +13,6 @@ import EmptyScreenMessage from '../components/EmptyScreenMessage';
 function OwnerHomeScreen({navigation, props}) {
 
   const [restaurants, setRestaurants] = useState([]);
-  const [triggerSearch, setTrigggerSearch] = useState(false);
   const isFocused = useIsFocused();
 
   const ownerId = useSelector((state) => {
@@ -32,33 +31,23 @@ function OwnerHomeScreen({navigation, props}) {
       return () => {
         setRestaurants([]);
       }
-    }, [isFocused ])
+    }, [isFocused])
   );
 
   const onCreateRestaurantPressed = (event) => {
     navigation.navigate(ROUTES.CREATE_RESTAURANT_STACK)
   }
 
-  const onRestaurantMenuPressed = (event) => {
-    console.log("On Restaurant Menu Press");
-    navigation.navigate(ROUTES.MENU_RESTAURANT_OWNER_STACK);
-  }
-
-  const onPhotoPresses = (event) => {
-    console.log('On Photo Pressed');
-    navigation.navigate(ROUTES.RESTAURANT_OWNER_PROFILE_STACK);
-  }
-
   return (
     <View>
     <View style={styles.global}>
-        {restaurants === undefined && 
+        {restaurants.length === 0 && 
          <EmptyScreenMessage
          message={CONSTANTS.SCREEN_TEXTS.NOT_RESTAURANTS}
          ></EmptyScreenMessage>
         }
         {
-          restaurants !== undefined && 
+          restaurants.length !== 0 && 
           <RestaurantFlatListOwner restaurants={restaurants}></RestaurantFlatListOwner>
         }
      
