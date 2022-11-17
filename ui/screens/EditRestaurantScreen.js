477db52b90e8fbcf64890af43e588ae26df694d9
candelaesquivel <candelaesquivelelizondo@outlook.com>
2022-11-17 17:29:15 -0300

    Edit restaurant screen refactorizado.

--- ui/screens/EditRestaurantScreen.js
+++ ui/screens/EditRestaurantScreen.js
@@ -1,25 +1,7 @@
-import { View, Text , ScrollView , StyleSheet , Dimensions} from 'react-native'
 import React, { useState } from 'react'
-import { colorPalette } from '../styles/colors'
-import I18n from "../../assets/localization/I18n";
-import { InputText } from '../components/InputText'
-import { MyButton } from '../components/button'
-import  Icon from 'react-native-vector-icons/MaterialIcons';
-import Carousal from '../components/carousal';
-import Mapa from '../components/mapa';
-import MyWeekButtons from '../components/WeekButton';
-import MyTimePicker from '../components/TimePicker';
-import { Theme } from '../styles/Theme';
-import CloseComponent from '../components/closeComponent';
-import { FoodTypesDropDown } from '../components/FoodTypesDropdown';
-import { PriceRangesDropdown } from '../components/PriceRangeDropdown';
-import { CONSTANTS } from '../../config';
+import { EditRestaurantScreenUI } from './restaurant/EditRestaurantScreenUI';
 
 function EditRestaurantScreen({navigation, props}) {
-
-  
-  const [openingHour,setOpeningHour] = useState(false);
-  const [closingHour,setClosingHour] = useState(false);
 
   const onCreateRestaurantPressed = (event) => {
     console.log('On Restaurant Create Press');
@@ -27,170 +9,12 @@ function EditRestaurantScreen({navigation, props}) {
   }
 
   return (
-    <ScrollView>
-     <Carousal></Carousal>
-      <View style={{flexDirection:"row-reverse"}}>
-        <Icon name = 'add' size={30} style={{marginRight: 10 , marginTop : "5%" }}></Icon>
-       </View>
-        <View style={styles.global}>
-
-                <View style={styles.globalTwo}>
-
-                    <Text style={styles.words}>
-                        {CONSTANTS.SCREEN_TEXTS.NAME_LABEL}    
-                    </Text>
-                    <InputText 
-                    placeholder="Mudra"
-                    color={colorPalette.White}
-                    placeholderTextColor = {colorPalette.Black}
-                    ></InputText>
-
-                    <Text style={styles.words}>
-                        {CONSTANTS.SCREEN_TEXTS.ADDRESS_LABEL}    
-                    </Text>
-                    <InputText 
-                    placeholder="Honduras 500"
-                    color={colorPalette.White}
-                    placeholderTextColor = {colorPalette.Black}
-                    ></InputText>
-
-                    <Text  style={styles.words} >
-                     {CONSTANTS.SCREEN_TEXTS.NEIGHBORHOOD_LABEL}    
-                    </Text>
-                    <InputText 
-                    placeholder="Palermo"
-                    color={colorPalette.White}
-                    placeholderTextColor = {colorPalette.Black}
-                    ></InputText>
-
-                    <Text style={styles.words}  >
-                    {CONSTANTS.SCREEN_TEXTS.LOCATION_LABEL}    
-                    </Text>
-                    <InputText 
-                    placeholder="CABA"
-                    color={colorPalette.White}
-                    placeholderTextColor = {colorPalette.Black}
-                    ></InputText>
-                    
-                    <Text style={styles.words}
-                    >
-                     {CONSTANTS.SCREEN_TEXTS.ZIP_CODE_LABEL}    
-                    </Text>
-                    <InputText 
-                    placeholder="1345"
-                    color={colorPalette.White}
-                    placeholderTextColor = {colorPalette.Black}
-                    ></InputText>
-
-                  <Text style={styles.words}>
-                    {CONSTANTS.SCREEN_TEXTS.HOUR_LABEL} 
-                    </Text>
-
-                </View>
-          <View style={styles.hour}>
-          
-                < MyButton
-                title= {CONSTANTS.SCREEN_TEXTS.OPEN_HOUR_LABEL} 
-                width={ Dimensions.get("window").width*0.5}
-                height={Dimensions.get("window").height*0.07}
-                onPress={() =>{ setOpeningHour(!openingHour)}}
-                ></MyButton>
-
-                < MyButton
-                title= {CONSTANTS.SCREEN_TEXTS.CLOSE_HOUR_LABEL} 
-                width={ Dimensions.get("window").width*0.5}
-                height={Dimensions.get("window").height*0.07}
-                onPress={() => {setClosingHour(!closingHour)}}
-                ></MyButton>
-              
-                  {
-                      openingHour && <MyTimePicker></MyTimePicker>
-                  }
-                  
-                  {
-                      closingHour &&  <MyTimePicker></MyTimePicker>
-                  }
-                  
-                </View>
-               
-          <Mapa></Mapa>
-          <CloseComponent>s</CloseComponent>
-
-          <View style ={styles.dropdownContainer}>
-            <FoodTypesDropDown></FoodTypesDropDown>
-          </View>
-          <View style ={styles.dropdownContainer}>
-              <PriceRangesDropdown></PriceRangesDropdown>
-          </View>
-          
-          <MyWeekButtons></MyWeekButtons>
-          
-          <View style={{flexDirection: 'column' , width : "100%",height : "60%", alignItems : "center" }}>
-            < MyButton
-                title= {CONSTANTS.SCREEN_TEXTS.SAVE_LABEL} 
-                width={ Dimensions.get("window").width*0.5}
-                height={Dimensions.get("window").height*0.07}
-                ></MyButton>
-
-                 < MyButton
-                title= {CONSTANTS.SCREEN_TEXTS.DELETE_RESTAURANT_LABEL} 
-                width={ Dimensions.get("window").width*0.5}
-                height={Dimensions.get("window").height*0.07}
-                ></MyButton>
-
-           </View>
-       
-      </View>
-
-      
-</ScrollView>
+    <EditRestaurantScreenUI
+ 
+    ></EditRestaurantScreenUI>
   )
 }
 
 
 export default EditRestaurantScreen;
 
-const styles = StyleSheet.create({
-  globalTwo:{
-    width:'90%', 
-    alignItems:'flex-start'
-  },
-  global:{
-    flexDirection : 'column',
-    alignItems : 'center'
-  },
-  words :{
-    fontSize: Theme.font.MEDIUM,
-    color: colorPalette.Black, 
-    marginLeft : "4%" , 
-    marginBottom : "3%"
-},
-hour : {
-  flexDirection : "column" , 
-  alignSelf : "center" ,
-  marginBottom :"5%"
-}
-,
-dropdownContainer: {
-  width : "90%" , 
-  marginBottom : 10  
- 
-},
-container: {
-  backgroundColor: colorPalette.White,
-  padding: 10,
-  width : "80%"
-},
-dropdown: {
-  height: 50,
-  borderColor: colorPalette.Orange,
-  borderRadius: 5,
-  paddingHorizontal: 10, borderWidth:1,
-},
- 
-placeholderStyle: {
-  fontSize: Theme.font.MEDIUM,
-  color : colorPalette.Black
-},
-
-});
