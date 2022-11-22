4f86cc9a2ea47fe6c6d360dc8bc94af99d34c242
BertiRean <tplaza15@gmail.com>
2022-11-22 15:21:57 -0300

    API: Get Restaurant Info solo devuelve lo del back

--- networking/endpoints/restaurant/getRestaurantInfo.js
+++ networking/endpoints/restaurant/getRestaurantInfo.js
@@ -8,8 +8,6 @@ export async function getRestaurantInfo(restaurantId){
   const URL = URL_SERVICES.RESTAURANT_DETAIL.replace('id', restaurantId);
 
   return axios.get(URL).then(resp => {
-    resp.data.averageRating = Number(resp.data.averageRating.$numberDecimal),
-    console.log(resp.data);
     return resp.data;
   }).catch(err => {
     console.error("WS Error: ", err);
