e5b523eeddc8d78ff7f53a53385d6dfd89b9b827
BertiRean <tplaza15@gmail.com>
2022-11-22 01:26:15 -0300

    UI: Preparando Error Msg Screen

---
+++ ui/screens/errors/ErrorMsgScreen.js
@@ -0,0 +1,12 @@
+import { ErrorMsgUI } from "./ErrorMsgUI"
+
+
+export const ErrorMsgScreen = ({navigation, props}) => {
+
+  return (
+    <ErrorMsgUI>
+
+    </ErrorMsgUI>
+  )
+
+}
---
+++ ui/screens/errors/ErrorMsgUI.js
@@ -0,0 +1,14 @@
+import { View } from "react-native"
+
+
+export const ErrorMsgUI = ({
+  props
+}) => {
+
+  return (
+    <View>
+      
+    </View>
+  )
+
+}
