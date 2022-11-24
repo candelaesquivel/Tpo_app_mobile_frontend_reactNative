1fb50c76c57d889c96eb0b6474d407f825e502fd
BertiRean <tplaza15@gmail.com>
2022-11-24 14:20:09 -0300

    Packages: Agregado Native Base

--- App.js
+++ App.js
@@ -2,14 +2,18 @@ import { NavigationContainer } from '@react-navigation/native';
 import MainNavigator from './navigation/MainNavigator';
 import { Provider } from 'react-redux'
 import store from './redux/store';
+import {NativeBaseProvider} from 'native-base'
 
 export default function App() {
   return (
-    <Provider store = {store}>
-      <NavigationContainer>
-        <MainNavigator></MainNavigator>
-      </NavigationContainer>
-    </Provider>
+    <NativeBaseProvider>
+      <Provider store = {store}>
+        <NavigationContainer>
+          <MainNavigator></MainNavigator>
+        </NavigationContainer>
+      </Provider>
+    </NativeBaseProvider>
+
   )
 }
 
