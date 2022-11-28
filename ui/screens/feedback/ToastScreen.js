e28a4a3fcd21c489e6440d3a9943a56c9f484d74
BertiRean <tplaza15@gmail.com>
2022-11-28 16:14:18 -0300

    UI: Toast - Creado un Custom Toast basados en states

--- ui/screens/feedback/ToastScreen.js
+++ ui/screens/feedback/ToastScreen.js
@@ -3,6 +3,7 @@ import { useEffect } from 'react';
 import { View } from 'react-native';
 import { useDispatch, useSelector } from 'react-redux';
 import { restoreToast } from '../../../redux/slices/feedBackReducer';
+import { Alert, Text, VStack, HStack } from 'native-base';
 
 export const ToastScreen = ({
 
@@ -13,12 +14,29 @@ export const ToastScreen = ({
   const status = useSelector(state => state.feedback.status);
   const dispatch = useDispatch();
 
+  const CustomToast = () => {
+    return (
+      <Alert w="100%" status={status}>
+        <VStack space={2} flexShrink={1} w="100%">
+          <HStack flexShrink={1} space={2} justifyContent="space-between">
+            <HStack space={2} flexShrink={1}>
+              <Alert.Icon mt="1" />
+              <Text fontSize="md" color="coolGray.800">
+                {message}
+              </Text>
+            </HStack>
+            </HStack>
+        </VStack>
+      </Alert>
+    )
+  }
+
   useEffect(() => {
 
     if (message.length > 0){
       toast.show({
-        title : message,
-        duration : 1000,
+        render : CustomToast,
+        duration : 1500,
         onCloseComplete : () => {
           dispatch(restoreToast());
         }
